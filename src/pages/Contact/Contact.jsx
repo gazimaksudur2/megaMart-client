382bb81a9ec8e9ca64c5f9b3a4cc94dfab58d081
GaziMaksudur <gazimaksudur2@gmail.com>
2024-08-29 12:30:20 +0600

    Contacts page established

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TitleBanner from '../../shared/TitleBanner';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
+    const { register, handleSubmit } = useForm()
+    const onSubmit = (data) => console.log(data)
     return (
         <div>
             <TitleBanner route={'home / contact'} title={'Contact'} />
-            This is contact page.
             <div className='my-10 w-[80%] mx-auto'>
                 <div className="overflow-hidden rounded-lg lg:col-span-2 h-96 lg:h-auto">
                     {/* <iframe width="100%" height="100%" className='min-h-full' frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"></iframe> */}
@@ -23,12 +25,65 @@ const Contact = () => {
                         className="rounded-lg shadow-lg"
                     ></iframe>
                 </div>
-                <div>
-                    <div>
-                        <h2>Contact Info</h2>
-                        <div>
-                            
+                <div className='my-20 grid grid-cols-6'>
+                    <div className='bg-base-200 p-8 rounded col-span-2'>
+                        <h2 className='text-xl mb-6 font-semibold font-open'>Contact Info</h2>
+                        <div className='space-y-3'>
+                            <div className='flex items-start justify-start '>
+                                <h3 className='min-w-[35%] font-semibold'>Phone:</h3>
+                                <div>
+                                    <h4>+880 1903 219313</h4>
+                                    <h4>+880 1731 013276</h4>
+                                </div>
+                            </div>
+                            <div className='flex items-start justify-start '>
+                                <h3 className='min-w-[35%] font-semibold'>Email:</h3>
+                                <div>
+                                    <h4>info@megamart.com</h4>
+                                    <h4>services@megamart.com</h4>
+                                </div>
+                            </div>
+                            <div className='flex items-start justify-start '>
+                                <h3 className='min-w-[35%] font-semibold'>Address:</h3>
+                                <div>
+                                    <h4>Store No: 206, Rajbari Market-3114</h4>
+                                    <h4>Sylhet Sadar, Sylhet</h4>
+                                </div>
+                            </div>
                         </div>
+                    </div>
+                    <div className='col-span-1'></div>
+                    <div className='bg-base-200 p-8 rounded col-span-3'>
+                        <form onSubmit={handleSubmit(onSubmit)} className='space-y-3'>
+                            <h2 className='capitalize mb-6 font-semibold text-xl'>Get in touch</h2>
+                            <div className='flex items-center justify-between gap-10'>
+                                <div className="form-control flex-1">
+                                    <label className="label">
+                                        <span className="label-text">First Name</span>
+                                    </label>
+                                    <input {...register("firstName", { required: true, maxLength: 20 })} type="text" placeholder="John" className="input input-bordered rounded" required />
+                                </div>
+                                <div className="form-control  flex-1">
+                                    <label className="label">
+                                        <span className="label-text">Last Name</span>
+                                    </label>
+                                    <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} type="text" placeholder="Doe" className="input input-bordered rounded" required />
+                                </div>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Subject</span>
+                                </label>
+                                <input {...register("subject", { pattern: /^[A-Za-z]+$/i })} type="text" placeholder="Write Your Subject" className="input input-bordered rounded" required />
+                            </div>
+                            <label className="form-control">
+                                <div className="label">
+                                    <span className="label-text">Message</span>
+                                </div>
+                                <textarea className="textarea textarea-bordered h-32 rounded" placeholder="Your Message"></textarea>
+                            </label>
+                            <input type="submit" className='btn btn-warning text-white w-full'/>
+                        </form>
                     </div>
                 </div>
             </div>
