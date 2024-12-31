d98fd923ed1357bb78a79f8f440dec3a6a9c94f0
gazimaksudur2 <gazimaksudur2@gmail.com>
2024-12-31 10:10:18 +0600

    daisyui require issue resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
   content: [
     "./index.html",
@@ -18,7 +19,7 @@ export default {
     },
   },
   plugins: [
-    require("daisyui"),
+    daisyui,
   ],
 }
 
